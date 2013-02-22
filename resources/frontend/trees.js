c19d0a93374cef1c4c05cd87e8f017ce7f0a8375
Mark Wilson <secretchinamark@gmail.com>
2013-02-21 19:17:10 -0800

    Just moving JS around

--- resources/frontend/trees.js
+++ resources/frontend/trees.js
@@ -1,99 +1,9 @@
-var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
-var map;
-
 // MARKERS or HEATMAP
 var mode = 'MARKERS';
 
+var map;
 var markers = [];
 var heatmap = new google.maps.visualization.HeatmapLayer();
-
-$('.mode-options a').click(function(e) {
-  var $clicked = $(e.currentTarget);
-
-  $('.mode-options a').removeClass('active');
-  $clicked.addClass('active');
-
-  var newMode = $clicked.data('mode');
-  if(mode !== newMode) {
-    clearTrees();
-    mode = newMode;
-    getTrees();
-  }
-});
-
-$.getJSON('/species')
-	.success(function(list) {
-
-		map = new google.maps.Map(document.getElementById('map-canvas'), {
-			center: sanFrancisco,
-			zoom: 13,
-			mapTypeId: google.maps.MapTypeId.SATELLITE
-		});
-
-		var adjustHeight = function() {
-			$('.trees-list').css('height', $('body').height() - 50);
-		};
-		adjustHeight();
-		$(window).resize(adjustHeight);
-
-		$('.trees-list').html('');
-		list.forEach(function(nameAndCount) {
-			$('.trees-list').append(
-				$([
-					'<a href="javascript:;" class="tree-option">',
-						'<span class="count">' + nameAndCount[1] + '</span>',
-						'<span class="name">' + nameAndCount[0] + '</span>',
-					'</a>'
-				].join(''))
-			);
-		});
-
-    // choose a random species with a reasonable number of trees to show
-    var candidates = $('.tree-option').filter(function(_, el) {
-      var count = parseInt($(el).children('.count').text(), 10);
-      return count <= 200 && count >= 100;
-    });
-    var $chosen = $(candidates[Math.floor(Math.random() * candidates.length)]);
-		$chosen.click();
-    // and scroll so that it's in view
-    $('.trees-container').scrollTop($chosen.offset().top - 100);
-
-	});
-
-$('.trees-list').delegate('.tree-option', 'click', function(e) {
-  var $clicked = $(e.currentTarget);
-	$clicked.toggleClass('active');
-
-  if($clicked.hasClass('active') && !$clicked.find('.image').length) {
-    var treeName = $clicked.children('.name').text();
-
-    //var species = treeName.split(' :: ')[0];
-    var species = treeName;
-
-    var $loading = $('<div class="image">...</div>').appendTo($clicked);
-
-    var getImg = function() {
-      $.ajax({
-        url: '/image',
-        data: {'species': species},
-        dataType: 'text',
-        success: function(url) {
-          var $img = $('<img class="image">').appendTo($clicked).hide();
-          $img.load(function() {
-            $loading.remove();
-            $img.show();
-          });
-          // if image 404s, try again for another random image.
-          $img.error(getImg);
-          $img.attr('src', url);
-        }
-      });
-    };
-    getImg();
-  }
-
-  getTrees();
-});
 
 function getTrees() {
   var selected = $('.trees-list .active')
@@ -139,3 +49,93 @@ function addTrees(data) {
 
   }
 }
+
+$('.mode-options a').click(function(e) {
+  var $clicked = $(e.currentTarget);
+
+  $('.mode-options a').removeClass('active');
+  $clicked.addClass('active');
+
+  var newMode = $clicked.data('mode');
+  if(mode !== newMode) {
+    clearTrees();
+    mode = newMode;
+    getTrees();
+  }
+});
+
+$('.trees-list').delegate('.tree-option', 'click', function(e) {
+  var $clicked = $(e.currentTarget);
+	$clicked.toggleClass('active');
+
+  if($clicked.hasClass('active') && !$clicked.find('.image').length) {
+    var treeName = $clicked.children('.name').text();
+
+    //var species = treeName.split(' :: ')[0];
+    var species = treeName;
+
+    var $loading = $('<div class="image">...</div>').appendTo($clicked);
+
+    var getImg = function() {
+      $.ajax({
+        url: '/image',
+        data: {'species': species},
+        dataType: 'text',
+        success: function(url) {
+          var $img = $('<img class="image">').appendTo($clicked).hide();
+          $img.load(function() {
+            $loading.remove();
+            $img.show();
+          });
+          // if image 404s, try again for another random image.
+          $img.error(getImg);
+          $img.attr('src', url);
+        }
+      });
+    };
+    getImg();
+  }
+
+  getTrees();
+});
+
+$.getJSON('/species')
+  .success(function(list) {
+
+    var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+      center: sanFrancisco,
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+
+    var adjustHeight = function() {
+      $('.trees-list').css('height', $('body').height() - 50);
+    };
+    adjustHeight();
+    $(window).resize(adjustHeight);
+
+    $('.trees-list').html('');
+    list.forEach(function(nameAndCount) {
+      $('.trees-list').append(
+        $([
+          '<a href="javascript:;" class="tree-option">',
+          '<span class="count">' + nameAndCount[1] + '</span>',
+          '<span class="name">' + nameAndCount[0] + '</span>',
+          '</a>'
+          ].join(''))
+        );
+    });
+
+    // choose a random species with a reasonable number of trees to show
+    var candidates = $('.tree-option').filter(function(_, el) {
+      var count = parseInt($(el).children('.count').text(), 10);
+      return count <= 200 && count >= 100;
+    });
+    var $chosen = $(candidates[Math.floor(Math.random() * candidates.length)]);
+    $chosen.click();
+    // and scroll so that it's in view
+    $('.trees-container').scrollTop($chosen.offset().top - 100);
+
+  });
